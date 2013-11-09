fa72c83576e75f14576e31edbd2fb6a43c4bb1c5
bredele <olivier.wietrich@gmail.com>
2013-11-09 00:05:59 -0700

    add some test

--- test/parser.js
+++ test/parser.js
@@ -1,6 +1,29 @@
 var parse = require('plugin-parser');
 var assert = require('assert');
 
-describe("parse(str)", function() {
+describe("parse(str)", function() 
+  it('should support "method"', function(){
+    var result = parse('method');
+  });
+
+  it('should support args', function(){
+    var result = parse('method:arg');
+  });
+
+  it('should support multiple args', function(){
+    var result = parse('method:arg1,arg2');
+  });
+
+  it('should support white spaces', function(){
+    var result = parse(' method : arg1, arg2');
+  });
+
+  it('should support just args', function(){
+    var result = parse('arg1,arg2');
+  });
+
+  it('should support multiple methods', function(){
+    var result = parse('method:arg1; method2');
+  });
   
 });
