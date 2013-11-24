306a472886b4605ddb854e72836af354083ff15c
bredele <olivier.wietrich@gmail.com>
2013-11-24 03:50:59 -0700

    first draft with default method

--- test/parser.js
+++ test/parser.js
@@ -6,39 +6,40 @@ function equal(obj1, obj2) {
 }
 
 describe("parse(str)", function() {
-  // it('should support "method"', function(){
-  //   var result = parse('method');
-  //   assert(equal(result, [{
-  //     method: "method",
-  //     params: []
-  //   }]));
-  // });
 
-  // it('should support args', function(){
-  //   var result = parse('method:arg');
-  //   assert(equal(result, [{
-  //     method: 'method',
-  //     params: ['arg']
-  //   }]));
-  // });
+  it('should support "method"', function(){
+    var result = parse('method');
+    assert(equal(result, [{
+      method: "method",
+      params: []
+    }]));
+  });
 
-  // it('should support multiple args', function(){
-  //   var result = parse('method:arg1,arg2');
-  //   assert(equal(result, [{
-  //     method: 'method',
-  //     params: ['arg1', 'arg2']
-  //   }]));
-  // });
+  it('should support args', function(){
+    var result = parse('method:arg');
+    assert(equal(result, [{
+      method: 'method',
+      params: ['arg']
+    }]));
+  });
 
-  // it('should support white spaces', function(){
-  //   var result = parse(' method : arg1,    arg2');
-  //   assert(equal(result, [{
-  //     method: 'method',
-  //     params: ['arg1', 'arg2']
-  //   }]));
-  // });
+  it('should support multiple args', function(){
+    var result = parse('method:arg1,arg2');
+    assert(equal(result, [{
+      method: 'method',
+      params: ['arg1', 'arg2']
+    }]));
+  });
 
-  // //special with default method
+  it('should support white spaces', function(){
+    var result = parse(' method : arg1,    arg2');
+    assert(equal(result, [{
+      method: 'method',
+      params: ['arg1', 'arg2']
+    }]));
+  });
+
+  //special with default method
   // it('should support just args', function(){
   //   var result = parse('arg1,arg2');
   //   assert(equal(result, [{
@@ -48,24 +49,32 @@ describe("parse(str)", function() {
   // });
 
 
-  // it('should support multiple methods', function(){
-  //   var result = parse('method:arg1; method2');
-  //   console.log(result);
-  //   assert(equal(result, [{
-  //     method: 'method',
-  //     params: ['arg1']
-  //   }, {
-  //     method: 'method2',
-  //     params : []
-  //   }]));
-  // });
+  it('should support multiple methods', function(){
+    var result = parse('method:arg1; method2');
+    assert(equal(result, [{
+      method: 'method',
+      params: ['arg1']
+    }, {
+      method: 'method2',
+      params : []
+    }]));
+  });
 
-    it('should support parse()', function(){
+  it('should support empty string and return default method', function(){
     var result = parse('');
     assert(equal(result, [{
       method: 'default',
       params: []
     }]));
   });
+
+  // it('parse(method:arg1,arg2;', function(){
+  //   var result = parse('method:arg1,arg2;');
+  //   console.log(result);
+  //   assert(equal(result, [{
+  //     method: 'method',
+  //     params: ['arg1', 'arg2']
+  //   }]));
+  // });
 
 });
